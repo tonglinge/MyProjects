cca3c6724152031c467bbc4a6175cbe84684db7a
tonglinge <songfreeman@sina.com>
2016-07-20 15:30:58 +0800

    Revert "CMDB"
    This reverts commit 136d388503f8b65a5135a3be58befc9fd2305ad9.

--- cmdb/client/plugins/plugin_api.py
+++
@@ -1,9 +0,0 @@
-from .linux import sysinfo
-
-
-def LinuxSysInfo():
-    return sysinfo.collect()
-
-def WindowsSysInfo():
-    from .windows import sysinfo as win_sysinfo
-    return win_sysinfo.collect()
